87e4e606546f589fe335f4816da5da20a97ca814
Abel h <abelhailu320@gmail.com>
2024-12-16 21:44:22 -0600

    first commit

---
+++ src/main/java/com/audio/audio_transcribe_ai/AudioTranscribeAiApplication.java
@@ -0,0 +1,13 @@
+package com.audio.audio_transcribe_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTranscribeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTranscribeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audio/audio_transcribe_ai/AudioTranscribeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audio_transcribe_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
